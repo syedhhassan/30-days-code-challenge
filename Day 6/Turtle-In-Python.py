cf46be24a3b399341b241d4211c16cda6b89abad
syedhhassan <86470591+syedhhassan@users.noreply.github.com>
2022-06-15 01:00:53 +0530

    Add files via upload

---
+++ Day 6/Turtle-In-Python.py
@@ -0,0 +1,15 @@
+import turtle as t
+
+h=t.Turtle()
+h.pencolor('red')
+h.backward(50)
+h.right(90)
+h.forward(50)
+h.left(90)
+h.forward(50)
+h.right(90)
+h.forward(50)
+h.right(90)
+h.forward(55)
+
+t.done()
